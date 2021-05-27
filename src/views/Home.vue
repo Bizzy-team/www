bd3306ede44894ad18c7c889e2cfd19c26c6845c
Kévin <kevin.joya@outlook.com>
2021-05-27 20:13:07 +0200

    Cleaned compo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <StartPage/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import StartPage from '../components/StartPage.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    StartPage,
   },
 };
 </script>
